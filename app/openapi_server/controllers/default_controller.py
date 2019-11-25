64b087665e35377e68ec03a9d812ea33df19849e
Bernie van Veen <b.vanveen@vwt.digital>
2019-11-25 10:39:28 +0100

    Added firstblob operation.

--- app/openapi_server/controllers/default_controller.py
+++ app/openapi_server/controllers/default_controller.py
@@ -1,11 +1,10 @@
 import logging
+import json
 from google.cloud import storage
 
 
 storage_client = storage.Client()
 storage_bucket = storage_client.get_bucket('vwt-d-gew1-mendix-wha-dojo')
-first_blob = storage_bucket.get_blob('source/hyrde/devices-locations/2019/11/01/20191101T000001Z.json')
-logging.info(f"First blob {first_blob.download_as_string()}")
 
 
 def travelinfo_get():  # noqa: E501
@@ -17,3 +16,10 @@ def travelinfo_get():  # noqa: E501
     :rtype: object
     """
     return 'do some magic!'
+
+
+def firstblob_get():
+    firstblob = storage_bucket.get_blob('source/hyrde/devices-locations/2019/11/01/20191101T000001Z.json')
+    json_data = json.loads(firstblob.download_as_string())
+    logging.info(f"First blob {json_data}")
+    return json_data
