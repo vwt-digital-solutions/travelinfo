20fa61f449f0747a9ae2e7ede888580def1d56ad
Bernie van Veen <b.vanveen@vwt.digital>
2020-02-12 08:23:25 +0100

    Prepared for coding dojo. DAT-1867

--- app/openapi_server/controllers/default_controller.py
+++ app/openapi_server/controllers/default_controller.py
@@ -21,57 +21,28 @@ with open(my_rainfile.name) as read_file:
     for row in my_reader:
         if row[0].startswith('#') or not row[2].strip() or row[2].strip() == '-1':
             continue
-        rain_dict[row[1]] = rain_dict.get(row[1], 0) + int(row[2].strip())
+        station = row[0].strip()
+        if station not in rain_dict:
+            rain_dict[station] = OrderedDict()
+        rain_dict[station][row[1]] = rain_dict[station].get(row[1], 0) + int(row[2].strip())
 
-print(rain_dict)
 
 work_blobs = list(storage_client.list_blobs(storage_bucket, prefix='2019/10'))
 print(work_blobs)
-current_day = ''
-dagelijkse_storingen = OrderedDict()
-for work_blob in work_blobs:
-    if work_blob.name[0:10] != current_day:
-        current_day = work_blob.name[0:10]
-        work_data = json.loads(work_blob.download_as_string())
-        print(f"blob {work_blob.name} current_day {current_day}")
-
-        storingen = list(filter(lambda x: True if "storing" in x["Omschrijving"].lower() or "incident" in x["Omschrijving"].lower() else False, work_data["Rows"]))
-        today = work_blob.name[11:19]
-
-        dagelijkse_storingen[today] = len(storingen)
-
-print(dagelijkse_storingen)
-
-
-
-def travelinfo_get():  # noqa: E501
-    """travelinfo_get
-
-     # noqa: E501
-
-
-    :rtype: object
-    """
-    return 'do some magic!'
 
 
 def raininfluence_get():
-    for (key, value) in rain_dict.items():
-        
-    total_work =
-
     return {
           "info": [
             {
               "area": "NL",
-              "correlation": 0.5,
-              "covariance": 0.3,
-              "total_precipitation": 203,
-              "total_work": 12454
+              "correlation": None,
+              "covariance": None,
+              "total_precipitation": None,
+              "total_work": None
             }
           ]
         }
-
 
 
 def firstblob_get():
@@ -83,14 +54,7 @@ def firstblob_get():
 
 
 def raininfo_get():
-    rainfile = tempfile.NamedTemporaryFile()
-    storage_bucket.get_blob('KNMI_20200119.txt').download_to_file(rainfile)
-
-    return send_file(
-        rainfile.name,
-        mimetype='text/csv',
-        as_attachment=True,
-        attachment_filename='precipitation.csv')
+    return rain_dict
 
 
 def g4pp_get():
@@ -102,3 +66,14 @@ def g4pp_get():
         mimetype='text/csv',
         as_attachment=True,
         attachment_filename='4pp.csv')
+
+
+def travelinfo_get():  # noqa: E501
+    """travelinfo_get
+
+     # noqa: E501
+
+
+    :rtype: object
+    """
+    return 'do some magic!'
